59dd114fbf9987adb80d80c52cff68a64892471b
Douglas <75459253+dls76@users.noreply.github.com>
2024-01-06 21:25:25 -0300

    upd

--- js/script.js
+++ js/script.js
@@ -1,32 +1,18 @@
 
-const accordionList = document.querySelectorAll('.accordion-list dt')
+// const accordionList = document.querySelectorAll('.accordion-list dt')
 
-function activeAccordion() {
-    console.log(this)
-}
+// function activeAccordion() {
+//     console.log(this)
+// }
 
-accordionList.forEach((item)=>{
-    item.addEventListener('click', activeAccordion)
-})
+// accordionList.forEach((item)=>{
+//     item.addEventListener('click', activeAccordion)
+// })
 
-
-const dias = [...document.querySelector('.titulo-dia')]
-
+const dias = [...document.querySelectorAll('.titulo-dia')]
 
 dias.forEach((e)=>{
-    e.addEventListener('click', showTitle)
-}
-
-function showTitle(){
-    alert(this.title)
-}
-
-
-
-
-
-
-
-
-
-
+    e.addEventListener('click', ()=>{
+        alert(e.getAttribute('title'))
+    })
+})
